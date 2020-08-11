0f5e2540bbcd934de4c5619dd034ae50b16e42a5
lighthouse-dev <lighthouse2.dev@gmail.com>
2020-08-11 23:14:33 +0900

    5章まで

---
+++ src/main/java/com/example/tddjava/Franc.java
@@ -0,0 +1,19 @@
+package com.example.tddjava;
+
+public class Franc {
+
+  private final int amount;
+
+  public Franc(final int amount) {
+    this.amount = amount;
+  }
+
+  Franc times(int multiplier) {
+    return new Franc(amount * multiplier);
+  }
+
+  public boolean equals(Object object) {
+    Franc franc = (Franc) object;
+    return amount == franc.amount;
+  }  
+}
---
+++ src/main/java/com/example/tddjava/Money.java
@@ -0,0 +1,5 @@
+package com.example.tddjava;
+
+public class Money {
+  
+}
